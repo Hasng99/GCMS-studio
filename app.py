4ee3e77a0e45e1ece6efd9e12b392e19a2882e91
Acaxia316 <foodchemgpt@gmail.com>
2026-07-31 13:35:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ RESULT_COLUMN_LABELS = {
     "quality": "Quality",
     "profile_match": "Profile match",
     "parent_fatty_acid": "Parent FAs",
-    "inclusion_reason": "",
+    "inclusion_reason": "Supporting",
     "lower_alkane": "Lower alkane",
     "upper_alkane": "Upper alkane",
     "lower_rt": "Lower RT",
